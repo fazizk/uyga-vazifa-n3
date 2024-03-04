4e9124664012b0bb8dbaf939e040bff055b92cc7
fazizk <161124149+fazizk@users.noreply.github.com>
2024-03-04 14:12:14 +0500

    Add files via upload

---
+++ app.js
@@ -0,0 +1,79 @@
+alert('Assalomalaykum')
+alert('Maslahatim kirmang 2')
+alert('Kirmang deyabman 3')
+alert('|Kirmang kuyasiz 4')
+alert('Hoy kuyasiz 5')
+confirm('U yerda nima borligini aytaymi 1')
+confirm('Ulim 2')
+confirm('ARVOHLAR  3')
+confirm('JINLAR  4')
+confirm('VA HAKOZOLAR  5')
+prompt('KIRMANG LEKIN  1')
+prompt('USTOZ KIRMANG AYTIB QUYIB OLDIZDAN UTYAPMAN 2')
+prompt('KIRMANG KUYASIZ 3')
+prompt('ANIQ KUYASIZ  4')
+prompt('A N A    K U Y D I Z            menda ayb yuq 5')
+
+
+console.log('001');
+var num1 = "1"
+console.log('002');
+var num2 = "2"
+console.log('003');
+var num3 = "3"
+console.log('004');
+var num4 = "4"
+console.log('005');
+var num5 = "5"
+console.log('006');
+var num6 = "6"
+console.log('007');
+var num7 = "7"
+console.log('008');
+var num8 = "8"
+console.log('009');
+var num9 = "9"
+console.log('010');
+var num10 = "10"
+let name1 = "yusuf";
+console.log(name1);
+let name2 = "isobek";
+console.log(name2);
+let name3 = "bexruz";
+console.log(name3);
+let name4 = "davronbek";
+console.log(name4);
+let name5 = "ramazon";
+console.log(name5);
+let name6 = "ulugbek";
+console.log(name6);
+let name7 = "nazarbek";
+console.log(name7);
+let name8 = "shodibek";
+console.log(name8);
+let name9 = "Og'abek";
+console.log(name9);
+let name10 = "sulton ali";
+console.log(name10);
+const male = "acer"
+console.log(male);
+const male1 = "acer1"
+console.log(male1);
+const male2 = "acer2"
+console.log(male2);
+const male3 = "acer3"
+console.log(male3);
+const male4 = "acer4"
+console.log(male4);
+const male5 = "acer5"
+console.log(male5);
+const male6 = "acer6"
+console.log(male6);
+const male7 = "acer7"
+console.log(male7);
+const male8 = "acer8"
+console.log(male8);
+const male9 = "acer9"
+console.log(male9);
+const male10 = "acer10"
+console.log(male10);                        
